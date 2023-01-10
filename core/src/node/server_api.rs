f94fe3901ebdb8fd48d7e16c3f869d5014692393
metabynull <bynull@meta-secret.org>
2023-01-09 18:53:38 -0800

    core: implementing http appi client

--- core/src/node/server_api.rs
+++ core/src/node/server_api.rs
@@ -1,10 +1,7 @@
-use reqwest::Error;
+use reqwest::{Error, Response};
 use crate::models::UserSignature;
 
-use crate::sdk::api::{
-    membership::MembershipResponse, GenericMessage, JoinRequest, MetaPasswordsResponse, PasswordRecoveryClaimsResponse,
-    PasswordRecoveryRequest, RegistrationResponse, SecretDistributionDocData, UserSharesResponse, VaultInfoResponse,
-};
+use crate::sdk::api::{membership::MembershipResponse, GenericMessage, JoinRequest, MetaPasswordsResponse, PasswordRecoveryClaimsResponse, PasswordRecoveryRequest, RegistrationResponse, SecretDistributionDocData, UserSharesResponse, VaultInfoResponse, MetaPasswordRequest};
 
 const API_URL: &str = "https://api.meta-secret.org";
 
@@ -37,35 +34,129 @@ pub async fn get_vault(user_sig: &UserSignature) -> Result<VaultInfoResponse, Er
     Ok(json)
 }
 
-pub async fn decline(request: &JoinRequest) -> Result<MembershipResponse, Error> {
-    todo!("not implemented")
+pub async fn decline(join_request: &JoinRequest) -> Result<MembershipResponse, Error> {
+    let client = reqwest::Client::new();
+    let response = client
+        .post(format!("{}/decline", API_URL))
+        .header("Access-Control-Allow-Origin", API_URL)
+        .json(join_request)
+        .send()
+        .await?;
+
+    // Read the response body as a JSON object
+    let json: MembershipResponse = response.json().await?;
+    Ok(json)
 }
 
 pub async fn accept(request: &JoinRequest) -> Result<MembershipResponse, Error> {
-    todo!("not implemented")
+    let client = reqwest::Client::new();
+    let response = client
+        .post(format!("{}/accept", API_URL))
+        .header("Access-Control-Allow-Origin", API_URL)
+        .json(request)
+        .send()
+        .await?;
+
+    // Read the response body as a JSON object
+    let json: MembershipResponse = response.json().await?;
+    Ok(json)
 }
 
 pub async fn claim_for_password_recovery(request: &PasswordRecoveryRequest) -> Result<PasswordRecoveryRequest, Error> {
-    todo!("not implemented")
+    let client = reqwest::Client::new();
+    let response = client
+        .post(format!("{}/claimForPasswordRecovery", API_URL))
+        .header("Access-Control-Allow-Origin", API_URL)
+        .json(request)
+        .send()
+        .await?;
+
+    // Read the response body as a JSON object
+    let json: PasswordRecoveryRequest = response.json().await?;
+    Ok(json)
 }
 
 pub async fn find_password_recovery_claims(user_sig: &UserSignature) -> Result<PasswordRecoveryClaimsResponse, Error> {
-    todo!("not implemented")
+    let client = reqwest::Client::new();
+    let response = client
+        .post(format!("{}/findPasswordRecoveryClaims", API_URL))
+        .header("Access-Control-Allow-Origin", API_URL)
+        .json(user_sig)
+        .send()
+        .await?;
+
+    // Read the response body as a JSON object
+    let json: PasswordRecoveryClaimsResponse = response.json().await?;
+    Ok(json)
 }
 
 pub async fn distribute(secret_doc: &SecretDistributionDocData) -> Result<GenericMessage<String>, Error> {
-    todo!("not implemented")
+    let client = reqwest::Client::new();
+    let response = client
+        .post(format!("{}/distribute", API_URL))
+        .header("Access-Control-Allow-Origin", API_URL)
+        .json(secret_doc)
+        .send()
+        .await?;
+
+    // Read the response body as a JSON object
+    let json: GenericMessage<String> = response.json().await?;
+    Ok(json)
 }
 
 pub async fn find_shares(user_sig: &UserSignature) -> Result<UserSharesResponse, Error> {
-    todo!("not implemented")
+    let client = reqwest::Client::new();
+    let response: Response = client
+        .post(format!("{}/findShares", API_URL))
+        .header("Access-Control-Allow-Origin", API_URL)
+        .json(user_sig)
+        .send()
+        .await?;
+
+    // Read the response body as a JSON object
+    let json: UserSharesResponse = response.json().await?;
+    Ok(json)
 }
 
 pub async fn get_meta_passwords(user_sig: &UserSignature) -> Result<MetaPasswordsResponse, Error> {
-    todo!("not implemented")
+    let client = reqwest::Client::new();
+    let response: Response = client
+        .post(format!("{}/getMetaPasswords", API_URL))
+        .header("Access-Control-Allow-Origin", API_URL)
+        .json(user_sig)
+        .send()
+        .await?;
+
+    // Read the response body as a JSON object
+    let json: MetaPasswordsResponse = response.json().await?;
+    Ok(json)
+}
+
+pub async fn delete_meta_password(meta_pass_request: &MetaPasswordRequest) -> Result<MetaPasswordsResponse, Error> {
+    let client = reqwest::Client::new();
+    let response: Response = client
+        .post(format!("{}/deleteMetaPassword", API_URL))
+        .header("Access-Control-Allow-Origin", API_URL)
+        .json(meta_pass_request)
+        .send()
+        .await?;
+
+    // Read the response body as a JSON object
+    let json: MetaPasswordsResponse = response.json().await?;
+    Ok(json)
 }
 
 ///cloud
 pub async fn join_meta_cloud(user_sig: &UserSignature) -> Result<RegistrationResponse, Error> {
-    todo!("not implemented")
+    let client = reqwest::Client::new();
+    let response: Response = client
+        .post(format!("{}/joinMetaCloud", API_URL))
+        .header("Access-Control-Allow-Origin", API_URL)
+        .json(user_sig)
+        .send()
+        .await?;
+
+    // Read the response body as a JSON object
+    let json: RegistrationResponse = response.json().await?;
+    Ok(json)
 }
