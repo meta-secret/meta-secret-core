61c8537fa7538c17bf076b6f7c5869830a3bf691
metabynull <bynull@meta-secret.org>
2023-02-25 00:57:43 -0800

    Add FindQuery
    Signed-off-by: metabynull <bynull@meta-secret.org>

--- core/src/node/db.rs
+++ core/src/node/db.rs
@@ -1,6 +1,7 @@
 use async_trait::async_trait;
 
-use crate::models::{MetaVault, UserCredentials};
+use crate::models::{AeadAuthData, MetaPasswordDoc, MetaPasswordId, MetaVault, UserCredentials};
+use crate::shared_secret::shared_secret::UserShareDto;
 
 #[async_trait(? Send)]
 pub trait GenericRepo<T> {
@@ -10,6 +11,22 @@ pub trait GenericRepo<T> {
     async fn get(&self, key: &str) -> Result<Option<T>, Self::Error>;
 }
 
+#[async_trait(? Send)]
+pub trait FindQuery<T> {
+    type Error: std::error::Error;
+
+    async fn find(&self, key: &str) -> Result<Vec<T>, Self::Error>;
+}
+
 pub trait UserCredentialsRepo: GenericRepo<UserCredentials> {}
 
 pub trait MetaVaultRepo: GenericRepo<MetaVault> {}
+
+pub trait UserPasswordsRepo: GenericRepo<UserPasswordEntity> {}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct UserPasswordEntity {
+    pub meta_pass_id: MetaPasswordId,
+    /// Encrypted UserShareDto-s
+    pub shares: Vec<AeadAuthData>,
+}
