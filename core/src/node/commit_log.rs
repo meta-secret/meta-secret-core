497c19563bd287bbafab4d4642b2c21b20b53762
metabynull <bynull@meta-secret.org>
2023-03-18 00:19:32 -0700

    core: commit log

--- core/src/node/commit_log.rs
+++ core/src/node/commit_log.rs
@@ -2,16 +2,19 @@ use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
 #[derive(Clone, Debug, PartialEq, Default, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct DataEvent {
     pub key: Key,
     pub value: Value,
 }
 
 #[derive(Clone, Debug, PartialEq, Default, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct Key {
     pub ns: String,
     pub store: String,
     pub id: String,
+    pub event_type: EventType
 }
 
 pub enum EventType {
