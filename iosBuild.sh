ec83bcbe01691deb507a19eebfaebe0855b9dba5
Dmitry Kuklin <dmitry@meta-secret.org>
2023-05-08 01:14:25 +0300

    core: script for iOS lib building

---
+++ iosBuild.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -e
+
+# Setup
+BUILD_DIR=target
+mkdir -p $BUILD_DIR
+cd $BUILD_DIR
+
+# Build static libs
+for TARGET in \
+        aarch64-apple-ios x86_64-apple-ios aarch64-apple-ios-sim \
+        x86_64-apple-darwin aarch64-apple-darwin
+do
+    rustup target add $TARGET
+    # Apple's App Sandbox disallows SysV semaphores; use POSIX semaphores instead
+    cargo build -r --target=$TARGET
+done
+
+# Create XCFramework zip
+FRAMEWORK="MetaSecretCore.xcframework"
+LIBNAME=libmeta_secret_core_mobile.a
+mkdir -p mac-lipo ios-sim-lipo
+IOS_SIM_LIPO=ios-sim-lipo/$LIBNAME
+MAC_LIPO=mac-lipo/$LIBNAME
+
+lipo -create -output $IOS_SIM_LIPO \
+        ../target/aarch64-apple-ios-sim/release/$LIBNAME \
+        ../target/x86_64-apple-ios/release/$LIBNAME
+lipo -create -output $MAC_LIPO \
+        ../target/aarch64-apple-darwin/release/$LIBNAME \
+        ../target/x86_64-apple-darwin/release/$LIBNAME
+xcodebuild -create-xcframework \
+        -library $IOS_SIM_LIPO \
+        -library $MAC_LIPO \
+        -library ../target/aarch64-apple-ios/release/$LIBNAME \
+        -output $FRAMEWORK
+zip -r $FRAMEWORK.zip $FRAMEWORK
+
+# Cleanup
+rm -rf ios-sim-lipo mac-lipo $FRAMEWORK
