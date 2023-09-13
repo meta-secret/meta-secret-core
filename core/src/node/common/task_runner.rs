27d3aa9956096310628cb0f4f60c55195f8070c7
metabynull <bynull@meta-secret.org>
2023-09-12 22:00:26 -0700

    v2, making wasm unit test work: fix RustTaskRunner test

--- core/src/node/common/task_runner.rs
+++ core/src/node/common/task_runner.rs
@@ -1,46 +1,51 @@
 use std::future::Future;
+
 use async_trait::async_trait;
 
 #[async_trait(? Send)]
-pub trait TaskRunner{
+pub trait TaskRunner {
     async fn spawn(&self, future: impl Future<Output=()> + 'static);
 }
 
 #[cfg(test)]
 mod test {
+    use std::cell::RefCell;
     use std::future::Future;
-    use std::ops::Deref;
-    use std::sync::{Arc, Mutex};
+    use std::rc::Rc;
+
     use async_trait::async_trait;
-    use crate::node::common::task_runner::{TaskRunner};
+
+    use crate::node::common::task_runner::TaskRunner;
 
     pub struct RustTaskRunner {}
 
     #[async_trait(? Send)]
     impl TaskRunner for RustTaskRunner {
         async fn spawn(&self, future: impl Future<Output=()> + 'static) {
-            use tokio::task;
-            let _ = task::spawn_local(future).await;
+            let local = tokio::task::LocalSet::new();
+
+            local.spawn_local(async move {
+                future.await;
+            });
+
+            local.await;
         }
     }
 
     #[tokio::test]
     async fn spawn_test() {
-        let mutex = Arc::new(Mutex::new(false));
-        let mutex_2 = mutex.clone();
+        let shared_obj = Rc::new(RefCell::new(false));
+        let shared_obj_2 = shared_obj.clone();
 
         let runner = RustTaskRunner {};
         runner.spawn(async move {
             println!("1. Async task");
-
-            let mut executed = mutex_2.lock().unwrap();
-            *executed = true;
+            shared_obj.replace(true);
         }).await;
 
         println!("2. Main thread");
 
-        let executed = mutex.lock().unwrap();
-        let executed = executed.deref();
+        let executed = shared_obj_2.take();
         assert!(executed);
     }
 }
