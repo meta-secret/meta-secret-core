d8f36cbd665c39ef5429df781b27f77b36fd6c59
cypherkitty <bynull@meta-secret.org>
2025-03-30 15:42:09 -0700

    taskomatic: v0.1-alpha with API key configuration

---
+++ infra/k8s/tasks.py
@@ -0,0 +1,13 @@
+from invoke import task, Context
+
+CLUSTER_NAME = "meta-secret-dev"
+CONFIG_FILE = "k3d-config-dev.yaml"
+REGISTRY_NAME = "meta-secret-registry"
+REGISTRY_PORT = 5000
+
+@task
+def create_cluster(c: Context):
+    print(f"Creating k3d cluster '{CLUSTER_NAME}' with config '{CONFIG_FILE}'")
+    c.run(f"k3d cluster create --config {CONFIG_FILE}")
+
+
