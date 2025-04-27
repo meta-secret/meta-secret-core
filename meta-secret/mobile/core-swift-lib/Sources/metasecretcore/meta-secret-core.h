0daf6b3f66f7ec2be19ae00d8909729ec4adbe2e
Dmitry Kuklin <dmitry@meta-secret.org>
2025-04-28 00:39:59 +0300

    android lib implementation

--- meta-secret/mobile/core-swift-lib/Sources/metasecretcore/meta-secret-core.h
+++
@@ -1,9 +0,0 @@
-#include <stdarg.h>
-#include <stdbool.h>
-#include <stdint.h>
-#include <stdlib.h>
-
-/**
- * https://betterprogramming.pub/from-rust-to-swift-df9bde59b7cd
- */
-void hello_world(void);
