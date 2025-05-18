3afe6b5f5d81566e7e4a1693b8ddfd507504595f
cypherkitty <bynull@meta-secret.org>
2025-05-17 18:11:07 -0700

    web sockets: make synchronization reactive

--- meta-secret/meta-server/web-server/src/main.rs
+++ meta-secret/meta-server/web-server/src/main.rs
@@ -3,6 +3,7 @@ use axum::{Json, Router, routing::post};
 use http::{StatusCode, Uri};
 use serde_derive::Serialize;
 use std::sync::Arc;
+use tokio::sync::Mutex;
 
 use anyhow::Result;
 use axum::response::Html;
@@ -19,10 +20,12 @@ use tracing_subscriber::{EnvFilter, FmtSubscriber};
 use axum::extract::ws::{WebSocketUpgrade, WebSocket, Message};
 use axum::response::IntoResponse;
 use futures_util::{StreamExt, SinkExt};
+use tokio::sync::broadcast;
 
 #[derive(Clone)]
 pub struct MetaServerAppState {
     data_transfer: Arc<MetaServerDataTransfer>,
+    notifier: broadcast::Sender<String>,
 }
 
 #[tokio::main]
@@ -80,7 +83,8 @@ async fn main() -> Result<()> {
         });
     });
 
-    let app_state = Arc::new(MetaServerAppState { data_transfer });
+    let (notifier, _) = broadcast::channel::<String>(100);
+    let app_state = Arc::new(MetaServerAppState { data_transfer, notifier });
 
     info!("Creating router...");
     let app = Router::new()
@@ -124,25 +128,41 @@ pub async fn meta_request(
 
     let response = state.data_transfer.send_request(msg_request).await.unwrap();
 
+    // Notify all websocket subscribers about the state change
+    let _ = state.notifier.send("state_changed".to_string());
+
     Json(response)
 }
 
 // WebSocket echo handler
-async fn ws_handler(ws: WebSocketUpgrade) -> impl IntoResponse {
-    ws.on_upgrade(handle_socket)
+async fn ws_handler(
+    ws: WebSocketUpgrade,
+    State(state): State<Arc<MetaServerAppState>>,
+) -> impl IntoResponse {
+    let notifier = state.notifier.clone();
+    ws.on_upgrade(move |socket| handle_socket(socket, notifier))
 }
 
-async fn handle_socket(mut socket: WebSocket) {
-    while let Some(Ok(msg)) = socket.next().await {
+async fn handle_socket(socket: WebSocket, notifier: broadcast::Sender<String>) {
+    let (ws_sender, mut ws_receiver) = socket.split();
+    let ws_sender = Arc::new(Mutex::new(ws_sender));
+    let mut rx = notifier.subscribe();
+    let ws_sender_clone = ws_sender.clone();
+    // Spawn a task to forward broadcast messages to the websocket
+    let send_task = tokio::spawn(async move {
+        while let Ok(msg) = rx.recv().await {
+            let mut sender = ws_sender_clone.lock().await;
+            if sender.send(Message::Text(msg.into())).await.is_err() {
+                break;
+            }
+        }
+    });
+    // Echo incoming messages as before
+    while let Some(Ok(msg)) = ws_receiver.next().await {
         match msg {
-            Message::Text(text) => {
-                let _ = socket.send(Message::Text(text)).await;
-            }
-            Message::Binary(bin) => {
-                let _ = socket.send(Message::Binary(bin)).await;
-            }
             Message::Ping(p) => {
-                let _ = socket.send(Message::Pong(p)).await;
+                let mut sender = ws_sender.lock().await;
+                let _ = sender.send(Message::Pong(p)).await;
             }
             Message::Close(_) => {
                 break;
@@ -150,4 +170,6 @@ async fn handle_socket(mut socket: WebSocket) {
             _ => {}
         }
     }
+    // If the client disconnects, stop the send task
+    send_task.abort();
 }
